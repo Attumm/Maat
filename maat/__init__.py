0963c0c07f1b99053c50cbcaf30154fd16cfd841
= <=>
2017-09-28 20:43:35 +0200

    Added __all__ for maat

--- maat/__init__.py
+++ maat/__init__.py
@@ -3,3 +3,8 @@ from validation import int_validation, str_validation, float_validation, list_va
 from validation import uuid_validation
 
 from extras import validate_args
+
+scale = maat_scale
+
+__all__ = ['scale', 'maat_scale', 'validate_args', 'Invalid', 'registered_functions',
+          'int_validation', 'str_validation', 'float_validation', 'list_validation', 'dict_validation', 'uuid_validation']
